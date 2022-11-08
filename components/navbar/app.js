89743bbd4b490fb9b9fe73ba75f4f9038b7273f2
Salmonte <almontesauel@gmail.com>
2022-11-07 20:54:19 -0500

    Implementing JavaScript

--- components/navbar/app.js
+++ components/navbar/app.js
@@ -46,10 +46,10 @@ async function generateRandomUser(){
     console.log('Local Data:', typeof localData)
     console.log('Local Data:', localData)
 
-    console.log('Local Data [Results Attributes - typeof]:', localData.results)
+    console.log('Local Data [Results Attributes - typeof]:', typeof localData.results)
     console.log('Local Data [Results Attributes]:', localData.results)
 
-    console.log('Remote Data:',typeof remoteData)
+    console.log('Remote Data:', typeof remoteData)
     console.log('Remote Data:', remoteData)
     //
     // console.log('new user generated')
