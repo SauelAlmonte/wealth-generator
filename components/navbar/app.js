b1f438fbfd5b5a83f967aed64f00af2b687ba9a5
Salmonte <almontesauel@gmail.com>
2022-11-27 22:36:19 -0500

    Wealth Generator Interactive

--- components/navbar/app.js
+++ components/navbar/app.js
@@ -8,4 +8,8 @@ console.log("Content Before:", allUser)
 
 allUser.push('string')
 
-console.log("Content After:", allUser)
+console.log("Content After 1st:", allUser)
+
+allUser.push(1)
+
+console.log("Content After 2nd:", allUser)
