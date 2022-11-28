c8f02212e7f5113e34e996fa9f05d449a20adb2d
Salmonte <almontesauel@gmail.com>
2022-11-28 00:41:00 -0500

    Wealth Generator Interactive

--- components/navbar/app.js
+++ components/navbar/app.js
@@ -1,19 +1,11 @@
 const addUserBtn = document.querySelector('#add-user')
 
-let allUser = [] // Arrays
+let data = [] // Arrays
 
-// Arrays add as much stuff("objects I'm adding")
+function addData(val){
+    data.push(val)
+}
 
-console.log("Content Before:", allUser)
+addData('12345')
 
-allUser.push('string') // Adding String
-
-console.log("Content After 1st:", allUser)
-
-allUser.push(1) // Adding a Number
-
-console.log("Content After 2nd:", allUser)
-
-allUser.push([3,4]) // Array & Adding a Number
-
-console.log("Content After 3rd:", allUser)
+console.log(data)
